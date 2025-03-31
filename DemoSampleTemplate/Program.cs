fcf0a2f3391728ae826c043d546155511ec60d32
Kiên Nguyễn <trungkien2691998@outlook.com>
2025-03-31 15:42:35 +0700

    Update Program.cs

--- DemoSampleTemplate/Program.cs
+++ DemoSampleTemplate/Program.cs
@@ -3,42 +3,25 @@ namespace DemoSampleTemplate
 {
     public class Program
     {
+        private static Microsoft.Extensions.Logging.ILogger _logger;
+
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
             var configuration = builder.Configuration;
-            builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
+            builder.Host.UseSerilog((context, services, configuration) =>
+                configuration.ReadFrom.Configuration(context.Configuration)
+                .ReadFrom.Services(services)
+                .Enrich.FromLogContext()
+                .WriteTo.Console());
+            builder.Host.ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseIISIntegration();
+                webBuilder.UseIIS();
+            });
             builder.Services.ConfigureServices(configuration);
             var app = builder.Build();
             app.Configure();
-
-            //try
-            //{
-            //    Log.Information("Application Starting up!");
-            //    CreateHostBuilder(args).Build().Run();
-            //}
-            //catch (Exception Ex)
-            //{
-            //    Log.Fatal(Ex, "Application failed to start properly");
-            //}
-            //finally
-            //{
-            //    Log.CloseAndFlush();
-            //}
         }
-
-        //public static IHostBuilder CreateHostBuilder(string[] args)
-        //{
-        //    return Host.CreateDefaultBuilder(args)
-        //        .UseSerilog((context, services, configuration) => configuration
-        //        .ReadFrom.Configuration(context.Configuration)
-        //        .ReadFrom.Services(services)
-        //        .Enrich.FromLogContext()
-        //        .WriteTo.Console())
-        //        .ConfigureWebHostDefaults(webBuilder =>
-        //        {
-        //            webBuilder.UseStartup<Startup>();
-        //        });
-        //}
     }
 }
